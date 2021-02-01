1b36dfed84377b2d2273816eb47f7e162cecc4c8
서교필 <gyofeel318@gmail.com>
2021-02-01 23:38:04 +0900

    feature: apply typed-vuex module to store

--- store/index.ts
+++ store/index.ts
@@ -1,3 +1,16 @@
+import { getAccessorType } from 'typed-vuex';
+import * as todolist from '~/store/todolist';
+
 export const state = () => ({
   userName: 'Gyopil'
 });
+
+export const indexAccessor = getAccessorType({
+  state
+})
+
+export const todolistAccessor = getAccessorType({
+  modules: {
+    todolist,
+  },
+})
