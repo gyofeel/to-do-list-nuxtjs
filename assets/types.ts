e2a6a56beb105cab352922a711d0661b1cdbb257
서교필 <gyofeel318@gmail.com>
2021-02-01 23:19:36 +0900

    feature: add todolist store

--- assets/types.ts
+++ assets/types.ts
@@ -1,1 +1,9 @@
 export type Priority = 'low' | 'medium' | 'high';
+
+export type Todo = {
+    id: string,
+    date: Date,
+    text: string,
+    priority: Priority,
+    isDone: boolean
+}
--- store/todolist.ts
+++ store/todolist.ts
@@ -1,4 +1,4 @@
-import { Priority } from '~/assets/types';
+import { Priority, Todo } from '~/assets/types';
 
 export const state = () => ({
 	todos: [],
@@ -6,11 +6,44 @@ export const state = () => ({
 });
 
 export const mutations = {
-	addTodo: (state:any, text: String, priority: Priority, isDone: Boolean) => {
-		state.todos.push(
+	addNewTodo: (state: any, text: string, priority: Priority, isDone: boolean) => {
+		const Todo: Todo = {
+			id: `${text}-${Date.now()}`,
+			date: new Date(),
 			text,
 			priority,
 			isDone
-		);
+		};
+		state.todos.push(Todo);
+
+		return Todo;
+	},
+	addTodo: (state: any, todo: Todo) => {
+		state.todos.push(todo);
+	},
+	addDone: (state: any, done: Todo) => {
+		state.dones.push(done);
+	},
+	removeTodo: (state: any, id: string) => {
+		return state.todos.splice(state.todos.findIndex((item: Todo) => item.id === id));
+	},
+	removeDone: (state: any, id: string) => {
+		return state.dones.splice(state.dones.findIndex((item: Todo) => item.id === id));
+	},
+	setPriority: (state: any, id: string, priority: Priority) => {
+		const idx = state.todos.findIndex((item: Todo) => item.id === id);
+		state.todos[idx].priority = priority;
+	},
+	setDoneTrue: ({ state, commit }: {state: any, commit: any}, id: string) => {
+		const idx = state.todos.findIndex((item: Todo) => item.id === id);
+		state.todos[idx].isDone = true;
+		commit('addDone', Object.assign({}, state.todos[idx]));
+		commit('removeTodo', state.todos[idx].id);
+	},
+	setDoneFalse: ({ state, commit }: {state: any, commit: any}, id: string) => {
+		const idx = state.dones.findIndex((item: Todo) => item.id === id);
+		state.dones[idx].isDone = false;
+		commit('addTodo', Object.assign({}, state.dones[idx]));
+		commit('removeDone', state.dones[idx].id);
 	}
 }
