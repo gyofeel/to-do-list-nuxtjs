d8ec96576bbca4e1a338d5a88bdffa08aecb9014
서교필 <gyofeel318@gmail.com>
2021-02-02 22:59:27 +0900

    feature:modify todolist store mutations

--- store/todolist.ts
+++ store/todolist.ts
@@ -1,12 +1,11 @@
 import { Priority, Todo } from '~/assets/types';
 
 export const state = () => ({
-	todos: [],
-	dones: []
+	todos: []
 });
 
 export const mutations = {
-	addNewTodo: (state: any, text: string, priority: Priority, isDone: boolean) => {
+	addTodo: (state: any, text: string, priority: Priority, isDone: boolean) => {
 		const Todo: Todo = {
 			id: `${text}-${Date.now()}`,
 			date: new Date(),
@@ -18,32 +17,15 @@ export const mutations = {
 
 		return Todo;
 	},
-	addTodo: (state: any, todo: Todo) => {
-		state.todos.push(todo);
-	},
-	addDone: (state: any, done: Todo) => {
-		state.dones.push(done);
-	},
 	removeTodo: (state: any, id: string) => {
 		return state.todos.splice(state.todos.findIndex((item: Todo) => item.id === id));
-	},
-	removeDone: (state: any, id: string) => {
-		return state.dones.splice(state.dones.findIndex((item: Todo) => item.id === id));
 	},
 	setPriority: (state: any, id: string, priority: Priority) => {
 		const idx = state.todos.findIndex((item: Todo) => item.id === id);
 		state.todos[idx].priority = priority;
 	},
-	setDoneTrue: ({ state, commit }: {state: any, commit: any}, id: string) => {
+	setDone: (state: any, id: string, done: boolean) => {
 		const idx = state.todos.findIndex((item: Todo) => item.id === id);
-		state.todos[idx].isDone = true;
-		commit('addDone', Object.assign({}, state.todos[idx]));
-		commit('removeTodo', state.todos[idx].id);
-	},
-	setDoneFalse: ({ state, commit }: {state: any, commit: any}, id: string) => {
-		const idx = state.dones.findIndex((item: Todo) => item.id === id);
-		state.dones[idx].isDone = false;
-		commit('addTodo', Object.assign({}, state.dones[idx]));
-		commit('removeDone', state.dones[idx].id);
+		state.todos[idx].isDone = done;
 	}
 }
